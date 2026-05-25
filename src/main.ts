2d73ba6accf098523f0031babd3d0ad2da274c13
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-05-25 12:23:43 -0700

    Add Apollo polling loop startup log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { runApolloListener } from "./listener/index.js";
 export { runApolloListener } from "./listener/index.js";
 
 if (import.meta.main) {
+  console.log("Apollo polling loop started...");
+
   const controller = new AbortController();
   process.on("SIGINT", () => controller.abort());
   process.on("SIGTERM", () => controller.abort());
